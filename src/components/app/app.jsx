b633051de1cf6171c19551f0557c4503efcfbed7
Art <ya.Artriches@ya.ru>
2020-08-04 20:04:59 +0300

    add: переменные для стилей - цвета - шрифты - тень

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import { createGlobalStyle } from 'styled-components';
+import Shipment from '../shipment/shipment.jsx';
 
 const GlobalStyle = createGlobalStyle`
-  body{
+  body {
     margin: 0;
     font-family: "Roboto", sans-serif;
+    font-style: normal;
+    font-weight: normal;
+    color: #C4C4C4;
     background-color: #e5e5e5;
   }
 `;
 
-const App = () => <GlobalStyle />;
+const App = () => <React.Fragment>
+    <GlobalStyle />
+    <Shipment />
+  </React.Fragment>;
 
 export default App;
---
+++ src/components/variables.styled.js
@@ -0,0 +1,37 @@
+export const ColorsActive = {
+  BLUE: '#215BF0',
+  DARK_BLUE: '#0F41C4',
+  RED: '#F12828'
+};
+
+export const ColorsBackground = {
+  GRAY: '#F6F6F6',
+  GRAY_DARK: '#E5E5E5'
+};
+
+export const ColorsMain = {
+  BLACK: '#222222',
+  GRAY: '#999999',
+  GRAY_LIGHT: '#EDEEEF',
+  WHITE: '#ffffff'
+};
+
+export const Shadow = `
+  background: #C4C4C4;
+  box-shadow: 0px 4px 16px rgba(0, 0, 0, 0.25);
+`;
+
+export const TextHeader = `
+  font-size: 32px;
+  line-height: 40px;
+`;
+
+export const TextDefault = `
+  font-size: 16px;
+  line-height: 24px;
+`;
+
+export const TextSmall = `
+  font-size: 12px;
+  line-height: 16px;
+`;
