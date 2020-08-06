661122885f5890148840174b7233f23fc0954c44
Art <ya.Artriches@ya.ru>
2020-08-06 19:19:27 +0300

    add: компанет входа App - глобальные стили

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,21 +1,36 @@
 import React from 'react';
-import { createGlobalStyle } from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
+import { ColorsBackground, TextDefault } from '../variables.styled';
 import Shipment from '../shipment/shipment.jsx';
 
 const GlobalStyle = createGlobalStyle`
   body {
+    display: flex;
+    align-items: center;
+    justify-content: center;
     margin: 0;
+    width: 100wh;
+    height: 100vh;
     font-family: "Roboto", sans-serif;
     font-style: normal;
     font-weight: normal;
     color: #C4C4C4;
-    background-color: #e5e5e5;
+    background-color: ${ColorsBackground.GRAY_DARK};
+    ${TextDefault}
   }
 `;
 
+const Wrapper = styled.section`
+  width: 1032px;
+  padding: 72px 88px 104px;
+  background-color: ${ColorsBackground.GRAY};
+`;
+
 const App = () => <React.Fragment>
-    <GlobalStyle />
+  <GlobalStyle />
+  <Wrapper className="shipment">
     <Shipment />
-  </React.Fragment>;
+  </Wrapper>
+</React.Fragment>;
 
 export default App;
