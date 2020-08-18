9f29dba70224d13e5ec3dd6d83b6457d4e13dc08
Art <ya.Artriches@ya.ru>
2020-08-18 19:52:05 +0300

    add: моки адресов самовывоза

---
+++ src/mock/pick-point.js
@@ -0,0 +1,14 @@
+const PickPoints = [
+  {
+    id: '1111',
+    title: 'Песчаная ул., д. 13',
+    coords: [55.977617, 37.160097]
+  },
+  {
+    id: '1112',
+    title: 'Подсосенский пер., д. 11',
+    coords: [55.970667, 37.165640]
+  }
+];
+
+export default PickPoints;
